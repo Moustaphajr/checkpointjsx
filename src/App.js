cadfb7f452b8be860b4d26a28e29cceaa1cce528
Moustapha <mouhamedjr2012@gmail.com>
2023-04-11 20:23:10 +0000

    first-commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Profile from './profile/profile';
+
+
+
+
+ function App(){
+  
+  return (
+    <div className="App">
+      <Profile 
+      fullName="Moustapha" 
+      bio="web developper"
+      profession="informaticien"
+      ame="1"
+      
+      >images</Profile>
+    
+    </div>
+  );
+}
+ 
+
+
+export default App;
